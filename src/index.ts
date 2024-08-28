39889b7781a4ad54b1fedc1309ad3ef87057adb1
ronis <ronis.tzol@gmail.com>
2024-08-28 17:08:29 +0100

    Initial commit. Set up nodejs with typescript. Serve html file with express.

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
